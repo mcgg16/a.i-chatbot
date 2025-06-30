b0c51dbad6ad642a513c9af777ebe7bbc896526a
Monica Guzman <a00838824@tec.mx>
2025-06-30 12:28:23 -0600

    Base for routes

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,21 @@ const app = express();
 app.use(cors()); // Will use the cors for middleware
 app.use(bodyParser.json()); // Will recieve requests in json format
 
+// Add routes, this one is basic GET
+app.get("/", (req, res) => {
+    res.send("Welcome to the AI Chatbot API!"); 
+})
+
+// Main route to connect to API with what it returns
+app.post("/message", (req, res) => {
+    res.json({
+        ai_response: "How are you?"
+    })
+})
+
+// Working with .env to give access to key 
+//process.env.OPENAI_API_KEY
+
 const PORT = process.env.PORT || 1330; // Will get por from env or default to 1330
 
 // Will be listening for events on the port
